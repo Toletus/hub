c74edf50813a8e6924d11d63c0435b8d3736a3c0
Edson Santos <edson@actuar.com>
2025-12-02 09:11:19 -0300

    refactor: make `Notifier` class static and add thread-safety with `lock` in notification operations

--- src/Toletus.Hub/Notifications/Notifier.cs
+++ src/Toletus.Hub/Notifications/Notifier.cs
@@ -2,38 +2,57 @@
 
 namespace Toletus.Hub.Notifications;
 
-public class Notifier
+public static class Notifier
 {
+    private static readonly Lock NotificationsLock = new();
     private static readonly List<Notification> Notifications = [];
 
     public static void AddNotification(string ip, int id, int command, DeviceType type)
     {
-        if (HasNotification(ip, command)) return;
+        lock (NotificationsLock)
+        {
+            if (HasNotification(ip, command, hasResponse: false))
+                return;
 
-        Notifications.Add(new Notification(ip, id, command, type));
+            Notifications.Add(new Notification(ip, id, command, type));
+        }
     }
 
     public static void UpdateNotification(string ip, int command, DeviceResponse deviceResponse)
     {
-        Notifications.FirstOrDefault(x => x.Ip == ip && x.Command == command)!.Response = deviceResponse;
+        lock (NotificationsLock)
+        {
+            Notifications.FirstOrDefault(x => x.Ip == ip && x.Command == command)!.Response = deviceResponse;
+        }
     }
 
     public static bool HasNotification(string ip, int command, bool hasResponse = true)
     {
-        return hasResponse
-            ? Notifications.Exists(x => x.Ip == ip && x.Command == command && x.Response != null)
-            : Notifications.Exists(x => x.Ip == ip && x.Command == command);
+        lock (NotificationsLock)
+        {
+            return hasResponse
+                ? Notifications.Exists(x =>
+                    x != null && x.Ip == ip && x.Command == command && x.Response != null)
+                : Notifications.Exists(x =>
+                    x != null && x.Ip == ip && x.Command == command);
+        }
     }
 
     public static Notification GetNotification(string ip, int command)
     {
-        return Notifications.FirstOrDefault(x => x.Ip == ip && x.Command == command)!;
+        lock (NotificationsLock)
+        {
+            return Notifications.FirstOrDefault(x => x.Ip == ip && x.Command == command)!;
+        }
     }
 
     public static void ClearNotification(string ip, int command)
     {
-        if (!HasNotification(ip, command)) return;
-
-        Notifications.Remove(GetNotification(ip, command));
+        lock (NotificationsLock)
+        {
+            var notification = Notifications.FirstOrDefault(x => x?.Ip == ip && x.Command == command);
+            if (notification != null)
+                Notifications.Remove(notification);
+        }
     }
 }
